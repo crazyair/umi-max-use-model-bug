7f65202c2feab95da739a6c9b9c0859725844178
叶枫 <645381995@qq.com>
2024-07-10 23:16:04 +0800

    feat: test

--- src/pages/Access/index.tsx
+++ src/pages/Access/index.tsx
@@ -1,21 +1,8 @@
-import { PageContainer } from '@ant-design/pro-components';
-import { Access, useAccess } from '@umijs/max';
-import { Button } from 'antd';
+// import { useModel } from '@umijs/max';
 
 const AccessPage: React.FC = () => {
-  const access = useAccess();
-  return (
-    <PageContainer
-      ghost
-      header={{
-        title: '权限示例',
-      }}
-    >
-      <Access accessible={access.canSeeAdmin}>
-        <Button>只有 Admin 可以看到这个按钮</Button>
-      </Access>
-    </PageContainer>
-  );
+  // const d = useModel('aaa');
+  return <>11</>;
 };
 
 export default AccessPage;
